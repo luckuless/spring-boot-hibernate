66722a20c0288669946b6d80c625b70b4697a3cd
luckuless <luckuless.rambojun@icloud.com>
2023-07-10 12:05:17 +0200

    repo setup

---
+++ src/test/java/be/luckuless/code/springboothibernate/SpringBootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package be.luckuless.code.springboothibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
